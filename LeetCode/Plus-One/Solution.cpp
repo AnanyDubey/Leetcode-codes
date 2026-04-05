9ed4b6c1c76c6dce1cb45ac3a4d98493ae6e59da
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-04-05 11:29:33 +0530

    Add solution for Plus One

--- LeetCode/Plus-One/Solution.cpp
+++ LeetCode/Plus-One/Solution.cpp
@@ -1,11 +1,11 @@
 1class Solution {
 2public:
 3    vector<int> plusOne(vector<int>& digits) {
-4        int carry = 0;
+4        //int carry = 0;
 5        int dsize = digits.size()-1;
 6        // digits[dsize] += 1;
 7        for(int i = dsize;i >= 0; i--){
-8            carry = 0;
+8            //carry = 0;
 9            if(digits[i]==9){
 10                digits[i] = 0;
 11            }else {
