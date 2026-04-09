bd9fa1ee878d4c0f33a95b3fedfefbc3633ab4b8
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-04-09 21:01:33 +0530

    Add solution for Roman to Integer

--- LeetCode/Roman-to-Integer/Solution.cpp
+++ LeetCode/Roman-to-Integer/Solution.cpp
@@ -2,24 +2,26 @@
 2public:
 3    int romanToInt(string s) {
 4        unordered_map<char, int> mp;
-5
-6        mp['I'] = 1;
-7        mp['V'] = 5;        
+5        
+6        mp['I'] = 1; 
+7        mp['V'] = 5;
 8        mp['X'] = 10;
-9        mp['L'] = 50;
-10        mp['C'] = 100;
-11        mp['D'] = 500;       
-12        mp['M'] = 1000;        
+9        mp['C'] = 100;
+10        mp['L'] = 50;
+11        mp['D'] = 500;
+12        mp['M'] = 1000;
 13
-14        int ans = 0;
-15        for(int i = 0; i < s.length(); i++){
-16            if(mp[s[i]] < mp[s[i+1]]){
-17                ans -= mp[s[i]];
-18            }
-19            else{
-20                ans += mp[s[i]];
-21            }
-22        }
-23    return ans;
-24    }
-25};
+14        int result = 0;
+15
+16        for(int i = 0; i < s.length(); i++){
+17            if(mp[s[i]] < mp[s[i+1]] ){
+18                result = result - mp[s[i]] ;
+19            }
+20            else{
+21                result = result + mp[s[i]];
+22            }
+23        }
+24    return result;
+25
+26    }
+27};
