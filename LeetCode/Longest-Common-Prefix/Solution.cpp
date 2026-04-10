61a8f7a25a1d705cadabcc59cfe040ecd8c54410
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-04-10 13:27:08 +0530

    Add solution for Longest Common Prefix

--- LeetCode/Longest-Common-Prefix/Solution.cpp
+++ LeetCode/Longest-Common-Prefix/Solution.cpp
@@ -1,24 +1,22 @@
 1class Solution {
 2public:
 3    string longestCommonPrefix(vector<string>& strs) {
-4        if (strs.empty()) return "";
-5
-6        string temp = strs[0];  // prefix string
+4
+5        string first = strs[0];
+6
 7
-8        for (int i = 1; i < strs.size(); i++) {
+8        for(int i = 1; i < strs.size(); i++){
 9            int j = 0;
 10
-11            // compare characters
-12            while (j < min(temp.size(), strs[i].size()) && temp[j] == strs[i][j]) {
-13                j++;
-14            }
-15
-16            temp = temp.substr(0, j);  // reduce prefix
-17
-18            if (temp == "") break;  // optimization
+11            while(j < min(first.size(), strs[i].size()) && first[j] == strs[i][j]){
+12                j++;
+13            }
+14
+15            first = first.substr(0,j);
+16
+17            if(first == "") break;
+18
 19        }
-20
-21        return temp;
-22    }
-23};
-24
+20        return first;
+21    }
+22};
