20224c58c9c496bf26adc5f5fd0755af06fc44d6
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-04-20 20:23:23 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,18 +1,20 @@
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4        int complement = 0;
-5        unordered_map<int,int> seen;
-6
-7        for(int i = 0; i < nums.size(); i++) {
-8            complement = target - nums[i];
-9
-10            if(seen.count(complement)){
-11                return { seen[complement], i };
-12            }
-13            
-14            seen[nums[i]] = i;
-15        }
-16    return {};
-17    }
-18};
+4        vector<int> target_indices;
+5        unordered_map<int, int> hash_table;
+6        for(int i = 0; i < nums.size(); i++){
+7            int second_integer = target - nums.at(i);
+8            //if(hash_table.find(second_integer) != hash_table.end()){
+9                if(hash_table.count(second_integer)){
+10                target_indices.push_back(i);
+11                target_indices.push_back(hash_table.find(second_integer)->second);
+12                break;
+13            }
+14            else{
+15                hash_table[nums.at(i)] = i;
+16            }
+17        }
+18    return target_indices;
+19    }
+20};
