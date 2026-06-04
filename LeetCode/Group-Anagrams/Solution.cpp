78af5beaacc9729cc27f8a714892f951e2556642
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-06-04 23:20:53 +0530

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.cpp
+++ LeetCode/Group-Anagrams/Solution.cpp
@@ -1,23 +1,38 @@
-1class Solution{
+1class Solution {
 2public:
 3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-4        
-5        map<string,vector<string>> m;
-6        
-7        for(auto x:strs) // for(int i=0;i<strs.size();i++) ;;; x = s[i] ;
-8        {
-9            auto s = x ;
-10            sort(s.begin(),s.end()) ;
-11           // reverse(s.begin(),s.end()) ;
-12            m[s].push_back(x) ;
-13        }
-14        
-15        vector<vector<string>> ans ;
-16        
-17        for(auto x:m)
-18            ans.push_back(x.second) ;
-19        
-20        return ans ;
-21        
-22    }
-23};
+4
+5        unordered_map<string, vector<string>> mp;
+6
+7        vector<int> count(26);
+8
+9        for(string s : strs)
+10        {
+11            fill(count.begin(), count.end(), 0);
+12
+13            for(char c : s)
+14            {
+15                count[c-'a']++;
+16            }
+17
+18            string key = "";
+19
+20            for(int i=0;i<26;i++)
+21            {
+22                key += "#";
+23                key += to_string(count[i]);
+24            }
+25
+26            mp[key].push_back(s);
+27        }
+28
+29        vector<vector<string>> result;
+30
+31        for(auto &x : mp)
+32        {
+33            result.push_back(x.second);
+34        }
+35
+36        return result;
+37    }
+38};
