0edc6ee019043a521b375e09094148119b2adef7
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-13 16:09:24 +0530

    Add solution for Remove Duplicates from Sorted List

--- LeetCode/Remove-Duplicates-from-Sorted-List/Solution.cpp
+++ LeetCode/Remove-Duplicates-from-Sorted-List/Solution.cpp
@@ -11,35 +11,30 @@
 11class Solution {
 12public:
 13    ListNode* deleteDuplicates(ListNode* head) {
-14        if(head==NULL || head->next == NULL) return head;
-15        
-16        ListNode* prev = head;
-17        ListNode* temp = prev->next;
-18
-19        // while(temp != NULL){ // && temp->next != NULL why this is imp ?
-20        //     if(prev->val == temp->val){
-21        //         //while(prev )
-22        //         prev->next = temp->next;
-23        //         temp = temp ->next;
-24        //     }
-25        //     else{
-26        //     prev= prev->next;
-27        //     //prev = temp;
-28        //     temp = temp -> next;
-29        //     }
-30        // }
-31
-32        while(temp!=NULL){
-33            while(temp!=NULL && prev->val == temp->val){
-34                temp = temp->next;
-35            }
-36            prev->next = temp;
-37            prev = temp;
-38            if(temp!=NULL) temp = temp->next;
-39        }
-40
-41        return head;      
-42    }
-43};
-44
-45//[1,2,3]
+14        if(head == NULL || head->next == NULL) return head;
+15
+16
+17        ListNode* prev = head;
+18        ListNode* temp = head->next;
+19
+20        while(temp != NULL){
+21            if(prev->val == temp->val){
+22
+23                prev->next = temp->next;
+24                temp = temp->next;
+25
+26            }
+27
+28            else{
+29
+30                prev = prev -> next;
+31                temp = temp -> next;
+32            }
+33        }
+34
+35            prev -> next = NULL;
+36
+37    return head;
+38
+39    }
+40};
