8a42c7ad2088e05ac9b5d6b152be6ac878e4d381
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-17 18:45:13 +0530

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.cpp
+++ LeetCode/Valid-Palindrome/Solution.cpp
@@ -1,26 +1,14 @@
 1class Solution {
 2public:
 3    bool isPalindrome(string s) {
-4        if(s.length()==0) return true;
+4        int left = 0, right = s.length() - 1;
 5
-6        int left = 0; int right = s.length() - 1;
-7
-8        while(left < right){
-9            while(left < right && !isalnum(s[left])){
-10                left++;
-11            }
-12            while(left < right && !isalnum(s[right])){
-13                right--;
-14            }
-15
-16            if(tolower(s[left]) != tolower(s[right])) return false;
-17        
-18        left++;right--;
-19        }
-20        
-21        return true;
-22
-23
-24
-25    }
-26};
+6        while(left < right){
+7            while(left < right && !isalnum(s[left])) left++;
+8            while(left < right && !isalnum(s[right])) right--;
+9            if(tolower(s[left]) != tolower(s[right])) return false;
+10            left++; right--;
+11        }
+12        return true;
+13    }
+14};
