d9fcd4ffae9044ff502406c3b62056052a1fd02b
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-17 16:00:32 +0530

    Add solution for Palindrome Number

--- LeetCode/Palindrome-Number/Solution.cpp
+++ LeetCode/Palindrome-Number/Solution.cpp
@@ -1,17 +1,12 @@
 1class Solution {
 2public:
 3    bool isPalindrome(int x) {
-4        int result = 0;
-5        int dummy = x;
-6        if(x<0) return false;
-7        while(dummy){
-8            int d = dummy%10;
-9            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && d > 7))
-10                return false;
-11            result = result * 10 + d;
-12            dummy/=10;
-13        }
-14        if(result == x) return true;
-15        else return false;
-16    }
-17};
+4        string s = to_string(x);
+5        int i = 0, j=s.size()-1;
+6        while(i<j){
+7            if(s[i++]!=s[j--])
+8            return false;
+9        }
+10        return 1;
+11    }
+12};
