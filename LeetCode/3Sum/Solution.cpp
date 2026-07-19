33496a797c300a70f50b55e4b8a172927fa8662a
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-19 19:41:11 +0530

    Add solution for 3Sum

--- LeetCode/3Sum/Solution.cpp
+++ LeetCode/3Sum/Solution.cpp
@@ -1,31 +1,31 @@
 1class Solution {
 2public:
 3    vector<vector<int>> threeSum(vector<int>& nums) {
-4        sort(nums.begin(), nums.end());
-5        vector<vector<int>> result;
-6
-7        for(int i = 0; i < nums.size() - 2  && nums[i]<=0; i++){
-8            if( i == 0 || nums[i]!=nums[i-1]) { twoSum(nums, i, result); } // Try with i+1 ;
-9
+4        vector<vector<int>> result;
+5        sort(nums.begin(), nums.end());
+6        for(int i = 0 ; i < nums.size()-2 && nums[i] <= 0; i++){
+7            if(i == 0 || nums[i] != nums[i-1]){
+8                twoSum(nums, i, result);
+9            }
 10        }
-11
-12
-13    return result;
-14    }
-15        void twoSum(vector<int>& nums, int i, vector<vector<int>>& result) {
-16            int left = i+1, right = nums.size() - 1;
-17
-18            while(left < right){
-19                int sum = nums[i] + nums[left] + nums[right];
-20                if(sum == 0){ 
-21                    result.push_back({nums[i], nums[left], nums[right]});left++;right-- ;
-22
-23                    while(left < right && nums[left] == nums[left - 1]) left++;
-24                    while(left < right && nums[right] == nums[right + 1]) right--;
-25                }
-26                else if( sum < 0) { left++; }
-27                else { right-- ;}
-28            }
-29
-30        }
+11        return result;
+12    }
+13
+14    void twoSum(vector<int>& nums, int i, vector<vector<int>>&result){
+15        int left = i + 1, right = nums.size()-1;
+16        
+17        while(left < right){
+18            int sum = nums[i] + nums[left] + nums[right];
+19            if(sum == 0){
+20                result.push_back({nums[i], nums[left], nums[right]});
+21                left++;right--;
+22                while(left < right && nums[left] == nums[left - 1]) left++;
+23                while(left < right && nums[right] == nums[right + 1]) right--;                
+24            }    
+25            else if(sum < 0) left++;
+26            else right--;
+27
+28        
+29        }     
+30    }
 31};
