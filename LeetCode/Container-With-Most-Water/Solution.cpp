8223d8f00b002674156a42670aed7509e2869efb
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-22 19:20:31 +0530

    Add solution for Container With Most Water

--- LeetCode/Container-With-Most-Water/Solution.cpp
+++ LeetCode/Container-With-Most-Water/Solution.cpp
@@ -5,8 +5,8 @@
 5        int area = 0;
 6        while(left < right){
 7            
-8            int dum = min(height[left], height[right]) * (right - left);
-9            if( dum > area) area = dum;
+8            area = max(area, min(height[left], height[right]) * (right - left));
+9            //if( dum > area) area = dum;
 10
 11            if(height[left] > height[right]) right--;
 12            else left++;        
