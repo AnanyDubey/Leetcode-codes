f288f1e03ce2ba340e91308851dfa91ac8b40dd8
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-23 17:21:11 +0530

    Add solution for Minimum Window Substring

--- LeetCode/Minimum-Window-Substring/Solution.cpp
+++ LeetCode/Minimum-Window-Substring/Solution.cpp
@@ -1,52 +1,42 @@
 1class Solution {
 2public:
 3    string minWindow(string s, string t) {
-4
-5        if (s.empty() || t.empty() || t.length() > s.length())
-6            return "";
-7
-8        unordered_map<char, int> need;
-9        unordered_map<char, int> window;
-10
-11        for (char c : t)
-12            need[c]++;
-13
-14        int required = need.size();
-15        int formed = 0;
-16
-17        int left = 0;
-18        int minLen = INT_MAX;
-19        int start = 0;
-20
-21        for (int right = 0; right < s.length(); right++) {
-22
-23            char c = s[right];
-24            window[c]++;
+4        if(s.length()==0 || t.length()==0 || t.length() > s.length()) return "";
+5
+6        int left = 0, minLen = INT_MAX;
+7        int found = 0;
+8
+9        unordered_map<char, int> need;
+10        unordered_map<char, int> window;
+11
+12        for(char c : t){
+13            need[c]++;
+14        }
+15
+16        int required = need.size(), start = 0;
+17
+18        for(int right = 0; right < s.length(); right++){
+19            char c = s[right];
+20            window[c]++;
+21
+22            if(need.count(c) && window[c] == need[c]){
+23                found++;
+24            }
 25
-26            if (need.count(c) && window[c] == need[c])
-27                formed++;
-28
-29            while (formed == required) {
-30
-31                int windowLen = right - left + 1;
-32
-33                if (windowLen < minLen) {
-34                    minLen = windowLen;
-35                    start = left;
+26            while(required == found){
+27                int currlen = (right - left) + 1;
+28                if(currlen < minLen){
+29                    minLen = currlen;
+30                    start = left;
+31                }
+32                window[s[left]]--;
+33
+34                if(need.count(s[left]) && window[s[left]] < need[s[left]]){
+35                    found--;
 36                }
-37
-38                char leftChar = s[left];
-39                window[leftChar]--;
-40
-41                if (need.count(leftChar) &&
-42                    window[leftChar] < need[leftChar]) {
-43                    formed--;
-44                }
-45
-46                left++;
-47            }
-48        }
-49
-50        return (minLen == INT_MAX) ? "" : s.substr(start, minLen);
-51    }
-52};
+37                left++;
+38            }
+39        }
+40    return (minLen == INT_MAX) ? "" : s.substr(start, minLen);
+41    }
+42};
