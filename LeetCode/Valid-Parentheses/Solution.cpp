5c9a426a25f0a43c9cd33b6e35e48000312e47aa
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-26 17:05:04 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.cpp
+++ LeetCode/Valid-Parentheses/Solution.cpp
@@ -1,29 +1,29 @@
 1class Solution {
 2public:
 3    bool isValid(string s) {
-4        int len = s.length();
-5        if(len <=1 ) return false;
-6
-7        stack<char> sta;
-8
-9        for(int i = 0; i < len; i++){
-10            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-11                sta.push(s[i]);
-12            }
-13            else if(s[i]==')' || s[i]=='}' || s[i]==']'){
-14                if(sta.empty()) 
-15                return false;
-16
-17            else if(s[i]==']' && sta.top()!='[') return false;
-18            else if(s[i]=='}' && sta.top()!='{') return false;
-19            else if(s[i]==')' && sta.top()!='(') return false;
-20
-21            else sta.pop();
-22            }
-23        }
-24
-25        if(sta.empty())
-26        return true;
-27        else return false;
+4        if(s.length() <=1) return false;
+5 
+6        stack<char> sta;
+7    for(int i = 0; i < s.length(); i++){
+8        if (s[i] == '(' || s[i] == '[' || s[i] == '{'){
+9            sta.push(s[i]);
+10        }
+11        else if (s[i] == ')' || s[i] == ']' || s[i] == '}'){
+12            if(sta.empty()) return false;
+13
+14            else if(sta.top() == '(' && s[i] != ')') return false;
+15            else if(sta.top() == '[' && s[i] != ']') return false;
+16            else if(sta.top() == '{' && s[i] != '}') return false;
+17            else sta.pop();
+18        }
+19        
+20        
+21    }
+22
+23    if(sta.empty()){
+24        return true;
+25    }
+26
+27    else return false;
 28    }
 29};
