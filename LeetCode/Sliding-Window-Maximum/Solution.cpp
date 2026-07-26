0a2cb07ef94ca8c348def5a28b16652430a9b08f
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-26 14:36:59 +0530

    Add solution for Sliding Window Maximum

--- LeetCode/Sliding-Window-Maximum/Solution.cpp
+++ LeetCode/Sliding-Window-Maximum/Solution.cpp
@@ -1,23 +1,24 @@
 1class Solution {
 2public:
 3    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-4        deque<int> dq;
-5        vector<int> ans;
-6        if(nums.empty() || k == 0) return {};
-7        for(int i = 0; i < nums.size();i++){
-8            if(!dq.empty() && dq.front() < i-k+1){
-9                dq.pop_front();
-10            }
-11
-12            while(!dq.empty() && nums[dq.back()] < nums[i]){
-13               dq.pop_back(); 
-14            }
-15            dq.push_back(i);
-16            
-17            if(i >= k-1){
-18            ans.push_back(nums[dq.front()]);
-19            }
-20        }
-21        return ans;
-22    }
-23};
+4        if(nums.size() < 1 || k == 0) return {};
+5        deque<int> dq;
+6        vector<int> ans;
+7
+8        for(int i = 0; i < nums.size(); i++){
+9            if(!dq.empty() && dq.front() < i-k+1){
+10                dq.pop_front();
+11            }
+12
+13            while(!dq.empty() && nums[dq.back()] < nums[i]){
+14                dq.pop_back();
+15            }
+16
+17            dq.push_back(i);
+18            if(i >= k-1){
+19            ans.push_back(nums[dq.front()]);
+20            }
+21        }
+22    return ans;
+23    }
+24};
