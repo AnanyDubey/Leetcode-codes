442f3a6cadd94f8cf158ee447e56bf01a88006e4
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-07-27 13:10:33 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.cpp
+++ LeetCode/Valid-Parentheses/Solution.cpp
@@ -1,29 +1,32 @@
 1class Solution {
 2public:
 3    bool isValid(string s) {
-4        if(s.length() <=1) return false;
-5 
-6        stack<char> sta;
-7    for(int i = 0; i < s.length(); i++){
-8        if (s[i] == '(' || s[i] == '[' || s[i] == '{'){
-9            sta.push(s[i]);
-10        }
-11        else if (s[i] == ')' || s[i] == ']' || s[i] == '}'){
-12            if(sta.empty()) return false;
-13
-14            else if(sta.top() == '(' && s[i] != ')') return false;
-15            else if(sta.top() == '[' && s[i] != ']') return false;
-16            else if(sta.top() == '{' && s[i] != '}') return false;
-17            else sta.pop();
-18        }
-19        
-20        
-21    }
-22
-23    if(sta.empty()){
-24        return true;
-25    }
-26
-27    else return false;
-28    }
-29};
+4        if(s.length()<=1) return false;
+5        unordered_map<char,char> pair;
+6
+7        pair['}'] = '{';
+8        pair[']'] = '[';
+9        pair[')'] = '(';
+10
+11        stack<char> sta;
+12
+13        for(int i = 0; i < s.length(); i++){
+14            if(s[i] == '[' || s[i] =='{' || s[i] =='('){
+15                sta.push(s[i]);
+16            }
+17            
+18            if(s[i] == ']' || s[i] =='}' || s[i] ==')'){
+19                    if (sta.empty())
+20                        return false;
+21                if(sta.top() != pair[s[i]]){
+22                    return false;
+23                }
+24            
+25                if(sta.top() == pair[s[i]]){
+26                    sta.pop();
+27                }
+28            }
+29        }
+30        if(sta.empty()) return true; else return false;
+31    }
+32};
