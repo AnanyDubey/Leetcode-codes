6a91fdff90e2e7fde85002d11f6be3d9c1c568fa
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-08-02 12:15:09 +0530

    Add solution for Car Fleet

--- LeetCode/Car-Fleet/Solution.cpp
+++ LeetCode/Car-Fleet/Solution.cpp
@@ -2,25 +2,23 @@
 2public:
 3    int carFleet(int target, vector<int>& position, vector<int>& speed) {
 4        vector<pair<int, double>> cars;
-5        int dum = 0;
-6        int fleets = 0 ; 
-7        double prevTime = 0;
-8        
-9        for(int i = 0; i < position.size(); i++){
-10            double time = (double)(target - position[i]) / speed[i]; 
-11            cars.push_back({position[i], time });
-12        }
-13        sort(cars.begin(), cars.end(), greater<pair<int, double>>()) ;
+5        double prevTime = 0;
+6        int fleets = 0;
+7
+8        for(int i = 0; i < position.size(); i++){
+9            double time = (target - position[i]) / (double)speed[i];
+10            cars.push_back({position[i], time});
+11        }
+12
+13        sort(cars.begin(), cars.end(), greater<pair<int, double>>());
 14        
-15        for(auto car : cars){
+15        for (auto car : cars){
 16            double currTime = car.second;
-17
-18            if(currTime > prevTime){
-19                fleets++;
-20                prevTime = currTime;
-21            }
-22
-23        }
-24    return fleets;
-25    }
-26};
+17            if(prevTime < currTime){
+18                fleets++;
+19                prevTime = currTime;
+20            }
+21        }
+22    return fleets;
+23    }
+24};
