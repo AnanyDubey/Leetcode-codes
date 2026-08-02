0fac297b094a1b45e44031e78441967a74e24f1a
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-08-02 17:51:08 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.cpp
+++ LeetCode/Valid-Anagram/Solution.cpp
@@ -1,17 +1,17 @@
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        if(s.size() != t.size())
-5        return false;
-6        int arr[26] {};
-7        for(int i = 0; i < s.size(); i++){
-8            arr[s[i] - 'a']++;
-9            arr[t[i] - 'a']--;
+4        if(s.length() != t.length()) return false;
+5        vector<int> ans(26, 0);
+6
+7        for(int i = 0; i < s.length(); i++){
+8            ans[s[i] - 'a']++;
+9            ans[t[i] - 'a']--;
 10        }
-11        for(int count : arr){
-12            if(count != 0) return false;
-13        }
-14    return true;
-15
+11
+12        for(int x : ans){
+13            if(x != 0) return false;
+14        }
+15    return true;
 16    }
 17};
