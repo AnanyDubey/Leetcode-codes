c446d440682068313c870d45bf9d5aefc2a212d4
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-08-02 12:14:58 +0530

    Add solution for Car Fleet

--- LeetCode/Car-Fleet/Solution.cpp
+++ LeetCode/Car-Fleet/Solution.cpp
@@ -1,11 +1,26 @@
-    def carFleet(self, target, position, speed):
-        n = len(speed)
-        arrivals = [(target-position[i])/speed[i] for i in range(n)]
-        data = sorted(list(zip(arrivals, position)), reverse=True)
-        count = 0
-        curr_max = -1
-        for ch in data:
-            if ch[1]>curr_max:
-                curr_max=ch[1]
-                count+=1
-        return count
+1class Solution {
+2public:
+3    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+4        vector<pair<int, double>> cars;
+5        int dum = 0;
+6        int fleets = 0 ; 
+7        double prevTime = 0;
+8        
+9        for(int i = 0; i < position.size(); i++){
+10            double time = (double)(target - position[i]) / speed[i]; 
+11            cars.push_back({position[i], time });
+12        }
+13        sort(cars.begin(), cars.end(), greater<pair<int, double>>()) ;
+14        
+15        for(auto car : cars){
+16            double currTime = car.second;
+17
+18            if(currTime > prevTime){
+19                fleets++;
+20                prevTime = currTime;
+21            }
+22
+23        }
+24    return fleets;
+25    }
+26};
