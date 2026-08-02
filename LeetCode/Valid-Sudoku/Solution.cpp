9736bf84ecf95347ab4fb6df330f2d421a3d2614
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-08-02 16:13:03 +0530

    Add solution for Valid Sudoku

--- LeetCode/Valid-Sudoku/Solution.cpp
+++ LeetCode/Valid-Sudoku/Solution.cpp
@@ -1,39 +1,38 @@
 1class Solution {
 2public:
 3    bool isValidSudoku(vector<vector<char>>& board) {
-4
+4        vector<unordered_set<char>> cols(9);
 5        vector<unordered_set<char>> rows(9);
-6        vector<unordered_set<char>> cols(9);
-7        vector<unordered_set<char>> boxes(9);
-8
-9        for(int r = 0; r < 9; r++) {
-10            for(int c = 0; c < 9; c++) {
-11
-12                char val = board[r][c];
-13
-14                // Empty cell
-15                if(val == '.')
-16                    continue;
-17
-18                // Check row
-19                if(rows[r].count(val))
-20                    return false;
-21                rows[r].insert(val);
-22
-23                // Check column
-24                if(cols[c].count(val))
-25                    return false;
-26                cols[c].insert(val);
+6        vector<unordered_set<char>> boxes(9);
+7
+8        for(int r = 0; r < 9; r++){
+9            for(int c = 0; c < 9; c++){
+10
+11                char val = board[r][c];
+12
+13                if(val == '.') continue;
+14
+15                if(rows[r].count(val)){
+16                    return false;
+17                }
+18                rows[r].insert(val);
+19
+20                if(cols[c].count(val)){
+21                    return false;
+22                }
+23                cols[c].insert(val);
+24
+25                int idx = 0;
+26                idx = (r/3) * 3 + c/3;
 27
-28                // Check 3x3 box
-29                int idx = (r / 3) * 3 + (c / 3);
-30
-31                if(boxes[idx].count(val))
-32                    return false;
-33                boxes[idx].insert(val);
+28                if(boxes[idx].count(val)){
+29                    return false;
+30                }
+31                boxes[idx].insert(val);
+32
+33            
 34            }
 35        }
-36
-37        return true;
-38    }
-39};
+36    return true;
+37    }
+38};
