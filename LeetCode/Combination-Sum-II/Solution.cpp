fc6de0b2a1b12c7054f424d7a5b2072c10a1da4e
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-09-07 17:44:16 +0530

    Add solution for Combination Sum II

--- LeetCode/Combination-Sum-II/Solution.cpp
+++ LeetCode/Combination-Sum-II/Solution.cpp
@@ -1,30 +1,29 @@
 1class Solution {
 2public:
-3    void f(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
-4    
-5        if(target == 0){
-6            ans.push_back(ds);
-7            return ;
-8        }
-9            
-10        for(int i = ind; i < arr.size(); i++){
-11            if(i > ind && arr[i] == arr[i-1]) continue;
-12            if(arr[i] > target) break;
-13            ds.push_back(arr[i]);
-14            f(i+1, target - arr[i], arr, ans, ds);
-15            ds.pop_back();
-16        }
+3    void f(int ind, vector<int> &arr, int target, vector<vector<int>> &ans, vector<int> ds){
+4        if(target == 0){
+5            ans.push_back(ds);
+6            return ;
+7        }
+8
+9        for(int i = ind; i < arr.size(); i++){
+10            if(i > ind && arr[i] == arr[i-1]) continue;
+11            if(arr[i] > target) break;
+12            ds.push_back(arr[i]);
+13            f(i+ 1, arr, target - arr[i], ans, ds);
+14            ds.pop_back();
+15        }
+16
 17    }
 18
 19
-20
-21public:
-22    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-23        sort(candidates.begin(), candidates.end());
-24        vector<vector<int>> ans;
-25        vector<int> ds;
-26        int s = 0;
-27        f(0, target, candidates, ans, ds);
-28        return ans;
-29    }
-30};
+20public:
+21    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+22        sort(candidates.begin(), candidates.end());
+23        vector<vector<int>> ans;
+24        vector<int> ds;
+25
+26        f(0, candidates, target, ans, ds);
+27        return ans;
+28    }
+29};
