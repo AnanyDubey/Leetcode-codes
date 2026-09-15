0bc3f0eee8cfb639c56dba63e46264f8ff521368
Anany Dubey <46838301+AnanyDubey@users.noreply.github.com>
2026-09-15 15:49:38 +0530

    Add solution for Combination Sum

--- LeetCode/Combination-Sum/Solution.cpp
+++ LeetCode/Combination-Sum/Solution.cpp
@@ -3,28 +3,27 @@
 3    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 4        vector<vector<int>> ans;
 5        vector<int> ds;
-6        //sort(candidates.begin(), candidates.end());
-7        f(0, candidates, target, ans, ds);
-8        return ans;
-9    }
-10
-11    void f(int ind, vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> &ds){
-12        
-13        if(target == 0){
-14            ans.push_back(ds);
-15            return;
-16        }
-17        //if(target < 0) return;
-18    
-19        for(int i = ind; i < candidates.size();i++){
-20            if (candidates[i] > target) continue;
-21            ds.push_back(candidates[i]);
-22            //sum = sum + ds[i]
-23            f(i, candidates, target-candidates[i], ans, ds);
-24            
-25
-26            ds.pop_back();
-27            
-28        }
-29    }
-30};
+6        f(0, candidates, target, ans, ds);
+7        return ans;
+8    }
+9
+10    void f(int ind, vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> &ds){
+11        
+12        if(target == 0){
+13            ans.push_back(ds);
+14            return;
+15        }
+16        if(target < 0) return;
+17    
+18        for(int i = ind; i < candidates.size();i++){
+19            ds.push_back(candidates[i]);
+20            //sum = sum + ds[i]
+21        
+22            f(i, candidates, target-candidates[i], ans, ds);
+23            ds.pop_back();
+24
+25            
+26            
+27        }
+28    }
+29};
